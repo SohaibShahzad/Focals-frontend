3007a7a5756e1e7178a6a7387fea3eaf25343780
SohaibShahzad <sohaib0919@gmail.com>
2023-05-08 17:14:44 +0500

    Resolved Error

---
+++ src/pages/dashboard/payment.js
@@ -0,0 +1,10 @@
+export default function UserPayments() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+          <h1 className="text-6xl font-bold">Payments</h1>
+        </main>
+      </div>
+    );
+  }
+  
